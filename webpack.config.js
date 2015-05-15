a4b5b7fdff69768fba3b6225813ffcc30b52d1a7
Sven Bohm <bohms@msu.edu>
2015-05-15 10:20:12 -0400

    optimized bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 var path = require('path');
-
+var node_modules = path.resolve(__dirname, 'node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 
 module.exports = {
   entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/main.js')],
+  resolve: {
+      alias: {
+            'react': pathToReact
+          }
+    },
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
-      loader: 'babel' // The module to load. "babel" is short for "babel-loader"
-    }]
+      noParse: [pathToReact]
   }
 };
